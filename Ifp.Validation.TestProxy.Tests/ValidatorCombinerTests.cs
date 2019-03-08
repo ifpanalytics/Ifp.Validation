54bbc3a49d558ecc7cd8c63e446243634b1fa6d4
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2019-03-08 14:15:48 +0000

    Add first test.

---
+++ Ifp.Validation.TestProxy.Tests/ValidatorCombinerTests.cs
@@ -0,0 +1,20 @@
+﻿using Ifp.Validation.TestProxy.Tests.SupportClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Ifp.Validation.TestProxy.Tests
+{
+    [TestClass]
+    public class ValidatorCombinerTests
+    {
+        [TestMethod()]
+        public void CombineRuleBasedValidationAndCollectionValidation()
+        {
+            var zoo = new Zoo();
+            var ruleBasedValidator = new RuleBasedValidator<Zoo>(new ValidationRuleDelegate<Zoo>(_ => ValidationOutcome.Success));
+            var collectionValidator = new SubCollectionValidator<Zoo, Animal>(z => z.Animals, new AnimalMustBeMaleRule());
+            var simpleRule = new ZooTestValidationRule(ValidationOutcome.Success);
+            var combined = new ValidatorCombiner<Zoo>(ruleBasedValidator, collectionValidator, simpleRule);
+        }
+    }
+}
