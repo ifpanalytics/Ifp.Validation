d4c14677d229e2b34048068c33916fe4b0a59236
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2019-03-09 10:28:20 +0000

    Convert to SDK project format

--- Ifp.Validation.TestProxy.Tests/Properties/AssemblyInfo.cs
+++ Ifp.Validation.TestProxy.Tests/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Ifp.Validation.TestProxy.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ifp.Validation.TestProxy.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2af005b2-1989-498d-8da7-64dfc3117847")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
