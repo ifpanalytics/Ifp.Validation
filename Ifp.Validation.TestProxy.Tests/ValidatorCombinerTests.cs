a4b372edbc57ccc3ba660248a9a778270664943c
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2019-03-09 09:29:15 +0000

    Extend ValidatorCombinerTests

--- Ifp.Validation.TestProxy.Tests/ValidatorCombinerTests.cs
+++ Ifp.Validation.TestProxy.Tests/ValidatorCombinerTests.cs
@@ -1,6 +1,5 @@
 ﻿using Ifp.Validation.TestProxy.Tests.SupportClasses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 
 namespace Ifp.Validation.TestProxy.Tests
 {
@@ -11,10 +10,20 @@ namespace Ifp.Validation.TestProxy.Tests
         public void CombineRuleBasedValidationAndCollectionValidation()
         {
             var zoo = new Zoo();
-            var ruleBasedValidator = new RuleBasedValidator<Zoo>(new ValidationRuleDelegate<Zoo>(_ => ValidationOutcome.Success));
-            var collectionValidator = new SubCollectionValidator<Zoo, Animal>(z => z.Animals, new AnimalMustBeMaleRule());
-            var simpleRule = new ZooTestValidationRule(ValidationOutcome.Success);
+            var r1WasCalled = false; var r2WasCalled = false; var r3WasCalled = false;
+            var ruleBasedValidator = new RuleBasedValidator<Zoo>(new ValidationRuleDelegate<Zoo>(_ =>
+            {
+                r1WasCalled = true;
+                return ValidationOutcome.Success;
+            }));
+            var collectionValidator = new SubCollectionValidator<Zoo, Animal>(z => z.Animals, new AnimalTestValidationRule(ValidationOutcome.Success, () => r2WasCalled = true));
+            var simpleRule = new ZooTestValidationRule(ValidationOutcome.Success, () => r3WasCalled = true);
             var combined = new ValidatorCombiner<Zoo>(ruleBasedValidator, collectionValidator, simpleRule);
+            var result = combined.Validate(new Zoo(new Dog()));
+            Assert.AreEqual(result.Severity, ValidationOutcome.Success.Severity);
+            Assert.AreEqual(r1WasCalled, true);
+            Assert.AreEqual(r2WasCalled, true);
+            Assert.AreEqual(r3WasCalled, true);
         }
     }
 }
