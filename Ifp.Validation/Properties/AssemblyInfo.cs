60ae550910f32713861f2ee3fea8ff76895b688a
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2019-03-09 12:11:48 +0000

    Remove duplicates from AssemblyInfo.cs and prepare release of 1.4

--- Ifp.Validation/Properties/AssemblyInfo.cs
+++ Ifp.Validation/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ifp")]
-[assembly: AssemblyProduct("Ifp.Validation")]
-[assembly: AssemblyTrademark("")]
+﻿using System.Runtime.CompilerServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: InternalsVisibleTo("Ifp.Validation.TestProxy.Tests")]
-[assembly: AssemblyTitle("Ifp.Validation")]
-[assembly: AssemblyDescription("Library for defining validation rules, combine them to sets of rules and test objects against that rules.")]
-[assembly: AssemblyCopyright("2017")]
-[assembly: AssemblyVersion("1.0.0.0")]
 
